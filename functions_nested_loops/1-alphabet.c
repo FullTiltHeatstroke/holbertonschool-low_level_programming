d439bac915d1e71fb2fb5ca5d3bc1524bf15f65f
rico <doodoo@gmail.com>
2024-10-25 13:07:35 +0000

    1added

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+*main - function prints text as output
+*
+*Return:return 0
+*/
+void print_alphabet (void)
+{
+  char abc;
+
+  for (abc = 'a'; abc < 'z'; abc++){
+    _putchar(abc); 
+    }
+  _putchar('\n'); 
+}
