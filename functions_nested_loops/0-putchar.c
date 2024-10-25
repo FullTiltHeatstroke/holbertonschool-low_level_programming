f8c84c3ee271c1e384dfde045e934c8204de551b
rico <doodoo@gmail.com>
2024-10-25 09:04:22 +0000

    aaa

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -6,16 +6,16 @@
 */
 int main(void)
 {
-  char type[10] = "_putchar";
+  char cool[10] = "_putchar";
 
   int a;
 
   for (a = 0; a < 8; a++)
     {
-      _putchar(type[a]);
+      _putchar(cool[a]);
       
     }
-  _putchar(\n); 
+  _putchar(10); 
   
   return (0); 
 } 
