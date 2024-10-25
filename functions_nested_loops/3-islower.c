ce4d545fafab3cd97e9787358a4576ac23466188
rico <doodoo@gmail.com>
2024-10-25 13:47:30 +0000

    3added

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*print_alphabet_x10 - prints alphabet letters
+* ten times
+*desc: The function uses loop to operate
+*Return: no return
+*/
+int _islower(int c);
+
+if (c>= 97 && c<= 122){
+
+  return (1);
+ }
+return (0); 
