e20fc9a303d6a6b27dc1bafec387ea0497974cae
rico <doodoo@gmail.com>
2024-10-28 09:58:03 +0000

    update previous

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -4,17 +4,24 @@
 void print_diagonal(int n)
 {
   int vert = 0;
-  int hor = 0; 
+  int hor = 0;
+
+if (n > 0)
+{   
   for (vert = 0; vert < n; vert++)
     {
       for (hor = 0; hor < n; hor++)
 	{
-	  if (vert == hor)
+	  if ( vert == hor)
 	    _putchar('\\');
-	  else if (vert > hor) 
-	    _putchar(' '); 
-
+	  else if (vert > hor)
+	    _putchar(' ');
+	  
 	}
       _putchar('\n');
     }
+}  
+  else
+    _putchar('\n');
+  
 }
