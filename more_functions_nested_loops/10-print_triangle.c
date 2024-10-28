2e885237af067c6f9963037b0662254a595d9d7d
rico <doodoo@gmail.com>
2024-10-28 12:38:02 +0000

    syntax

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -2,21 +2,26 @@
 
 void print_triangle(int size)
 {
-  int x = 0;
-  int y = 0; 
+    int x, y;
 
-    for (x = 0; x <= size; x++)
-      {
-	for (y = 0; y < 2 * (size - x) - 1; y++)
-	  {
-	  _putchar(' ');
-	  }
-	for (y = 0; y <= x; y++)
-	  {
-	    _putchar('#');
-	  }
-	_putchar('\n');
-      }
-    if (size <= 0)
-      _putchar('\n'); 
+    if (size <= 0) {
+        _putchar('\n');
+    }
+
+    for (x = 0; x < size; x++)
+    {
+     
+        for (y = 0; y < size - x - 1; y++)
+        {
+            _putchar(' ');
+        }
+
+        for (y = 0; y <= x; y++)
+        {
+            _putchar('#');
+        }
+
+        _putchar('\n');
+    }
 }
+
