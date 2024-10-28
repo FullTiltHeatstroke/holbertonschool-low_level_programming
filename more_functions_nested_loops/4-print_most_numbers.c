1c6ac9d5d7f68f5a0276fcae66c3233bd8dfeb44
rico <doodoo@gmail.com>
2024-10-28 04:59:51 +0000

    4added

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+
+
+
+void print_most_numbers(void)
+{
+  int i = 0;
+  for (i = '0'; i <= '9' ; i++)
+    {
+      if (i == '2' || i == '4')
+	{
+	  continue; 
+	}
+      _putchar(i);
+    }
+  _putchar('\n'); 
+}
