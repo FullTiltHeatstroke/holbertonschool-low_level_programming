adfd78bcd1715a3f66b36e4ae690b81c6d6d1dd7
rico <someone@gmail.com>
2024-10-31 10:54:04 +0000

    dododo

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -5,18 +5,14 @@
 void puts2(char *str)
 {
   int i = 0;
-  int count = 0;
 
-  while (str[count] != '\0')
-    {
-      count++; 
-    }
-  while (i < count)
-    {
 for (i = 0; str[i] != '\0'; i += 2)
   {
+    if ( i % 2 == 0)
+      {
    _putchar(str[i]);
+      }
   }
-  }
+
   _putchar('\n'); 
 }
