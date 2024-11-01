d944774c1d839b19a2e3b6a000b4119819a7f64b
rico <someone@gmail.com>
2024-11-01 06:22:51 +0000

    7added

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -1,2 +1,39 @@
 #include "main.h"
 
+
+
+void puts_half(char *str)
+{
+  int count = 0;
+  int middle;
+  int n; 
+
+  while (str[count] == '\0')
+    {
+      count++; 
+
+    }
+  if (count % 2 == 0)
+    {
+
+      for (middle = count / 2; str(count) != '\0'; middle++)
+	{
+
+	  _putchar(str[middle]); 
+
+	}
+
+    } else if (count % 2)
+    {
+
+      for (n = (count - 1) / 2; n < count - 1; n++)
+	{
+
+	  _putchar(str[n + 1]); 
+
+	}
+
+    }
+  _putchar('\n'); 
+
+}
