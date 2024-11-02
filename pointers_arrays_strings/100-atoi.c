d7eb41ac50a0f1c0d982eb45d5b6220a6e416190
rico <someone@gmail.com>
2024-11-02 10:05:03 +0000

    finish

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -3,18 +3,27 @@ int _atoi(char *s)
 {
  unsigned int num = 0;
   int i = 0;
-  int neg = 1; 
+  int neg = 1;
+  int stop = 0; 
 
   while (s[i] != '\0')
     {
-      if (s[i] >= '0' && s[i] <= '9')
+      if (s[i] == '-')
 	{
-	  if (*s == '-')
+	  if (!stop)
 	    {
-	      neg *= -1; 
+	      neg *= -1;
 	    }
-	  num = num * 10 + (s[i] -'0'); 
 	}
+	  else if ( s[i] >= '0' && s[i] <= '9')
+	    {
+	  num = num * 10 + (s[i] -'0');
+	  stop = 1;
+	    }
+	  else if(stop)
+	    {
+	      break;
+	    }
       i++; 
     } 
       
