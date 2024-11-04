2b981fdf0781adef7a407197be8993dbf972a945
rico <someone@gmail.com>
2024-11-04 12:53:36 +0000

    3added

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -7,21 +7,18 @@
 int _strcmp(char *s1, char *s2)
 {
 
-  int count1 = 0;
-  int count2 = 0; 
+  int count = 0; 
 
-  while (s1[count1] != '\0' && s2[count2] != '\0')
+  while (s1[count] != '\0' && s2[count] != '\0')
     {
-      count1++;
-      count2++;
+      if (s1[count] != s2[count])
+	{
+      return (s1[count] - s2[count]);
+	}
+      count++; 
     }
-  if (s1[count1] == s2[count2])
-    {
-      return (0);
-    }
-  else
-    {
-      return (s1[count1] - s2[count2]);
-    }
+    
+     return (s1[count] - s2[count]); 
+    
       
 }
