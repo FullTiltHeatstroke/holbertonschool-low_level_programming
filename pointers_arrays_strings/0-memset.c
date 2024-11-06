9d4863e96e63d3b6d1cdd3d0ba6e5ee2ab7f93b1
rico <someone@gmail.com>
2024-11-06 11:40:01 +0000

    0added

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,1 +1,20 @@
 #include "main.h"
+
+char *_memset(char *s, char b, unsigned int n)
+{
+
+ unsigned int count = 0; 
+  
+  while (s[count] != '\0')
+    {
+      count++;
+      if (count < n)
+	{
+      s[count] = b;
+	}
+    }
+  return(s); 
+
+
+
+}
