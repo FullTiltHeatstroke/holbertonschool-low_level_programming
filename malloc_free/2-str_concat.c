7df54a823ef99c75e1f071271e57307766918a6b
rico <someone@gmail.com>
2024-11-12 08:14:00 +0000

    syntax

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -13,32 +13,25 @@ char *str_concat(char *s1, char *s2)
   int i = 0;
   char *concat;
 
-    if (s1 == NULL)
+  if (s1 == NULL)
         s1 = "";
-    if (s2 == NULL)
+  if (s2 == NULL)
         s2 = "";
 
-   
-    while (s1[lens1])
+  while (s1[lens1])
         lens1++;
-    while (s2[lens2])
+  while (s2[lens2])
         lens2++;
+  concat = malloc(sizeof(char) * (lens1 + lens2 + 1));
+  if (concat == NULL)
+    return (NULL);
 
-   
-    concat = malloc(sizeof(char) * (lens1 + lens2 + 1));
-    if (concat == NULL)
-        return (NULL);
+  for (i = 0; i < lens1; i++)
+    concat[i] = s1[i];
 
-   
-    for (i = 0; i < lens1; i++)
-        concat[i] = s1[i];
-
-   
-    for (i = 0; i < lens2; i++)
+  for (i = 0; i < lens2; i++)
         concat[lens1 + i] = s2[i];
 
-   
-    concat[lens1 + lens2] = '\0';
-
-    return (concat);
+  concat[lens1 + lens2] = '\0';
+  return (concat);
 }
