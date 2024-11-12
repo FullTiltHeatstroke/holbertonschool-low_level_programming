840da3598d608c76ddec667f9dd97f18f3f32c5a
rico <someone@gmail.com>
2024-11-12 08:11:10 +0000

    2fix

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -8,12 +8,37 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-  int len = 0;
-  char concat; 
-  
-  if (str_concat == NULL)
-    return (' ');
+  int lens1 = 0;
+  int lens2 = 0;
+  int i = 0;
+  char *concat;
 
-  concat = malloc (sizeof (char)(len - 1)); 
+    if (s1 == NULL)
+        s1 = "";
+    if (s2 == NULL)
+        s2 = "";
 
+   
+    while (s1[lens1])
+        lens1++;
+    while (s2[lens2])
+        lens2++;
+
+   
+    concat = malloc(sizeof(char) * (lens1 + lens2 + 1));
+    if (concat == NULL)
+        return (NULL);
+
+   
+    for (i = 0; i < lens1; i++)
+        concat[i] = s1[i];
+
+   
+    for (i = 0; i < lens2; i++)
+        concat[lens1 + i] = s2[i];
+
+   
+    concat[lens1 + lens2] = '\0';
+
+    return (concat);
 }
