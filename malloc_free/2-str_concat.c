342ca76d19013373d20f9ba78d573ca727963519
rico <someone@gmail.com>
2024-11-12 06:48:42 +0000

    2added

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+char *str_concat(char *s1, char *s2)
+{
+  int len = 0;
+  char concat; 
+  
+  if (str_concat == NULL)
+    return (' ');
+
+  concat = malloc (sizeof (char)(len - 1)); 
+
+}
