bcf6516e5fb6552a935f46f58777dcc1ab9d595c
rico <someone@gmail.com>
2024-11-15 04:37:29 +0000

    1added

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -9,6 +9,12 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
  unsigned int j = 0;
   char *new_str; 
 
+  if (s1 == NULL)
+    s1 = "";
+  
+  if (s2 == NULL)
+    s2 = ""; 
+  
   while (s1[len1])
     len1++;
   while (s2[len2])
@@ -17,21 +23,18 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
     n = len2;
 
   new_str = malloc(len1 + n + 1);
+  if (new_str == NULL)
+    return (NULL); 
 
-    for (i = 0; i < len1; i++)
-      {
+  for (i = 0; i < len1; i++)
+    {
       new_str[i] = s1[i];
-      }
-  for (j =0; j < len2; j++)
+    } 
+  for (j =0; j <= n; j++)
     {
     new_str[i + j] = s2[j];
     }
-  new_str[i+j] = '\0'; 
-
-    return (new_str); 
-    
-    
-
-
-
+  
+new_str[i+j] = '\0'; 
+return (new_str); 
 }
