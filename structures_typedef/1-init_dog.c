7621c58263c2033112cb85466ad887452ca49c7d
rico <someone@gmail.com>
2024-11-17 13:16:01 +0000

    syntax

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -9,11 +9,12 @@
  *
  * initilizes members of struct dog
  */
+
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-  if (d == NULL)
-    return;
-  d->name = name; 
-  d->age = age;
-  d->owner = owner; 
+if (d == NULL)
+return;
+d->name = name;
+d->age = age;
+d->owner = owner;
 }
