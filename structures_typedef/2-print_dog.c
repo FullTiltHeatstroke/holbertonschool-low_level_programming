2d693feb9a7f30caedd2bd822e5d1bcb9e874dd6
rico <someone@gmail.com>
2024-11-18 03:31:36 +0000

    syntax

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -5,16 +5,13 @@
  * @d: struct to initialize
  * Return: Nothing.
  */
-
 void print_dog(struct dog *d)
 {
-  if (!d)
-    return;
-  
-  if (d->name == NULL)
-    d->name = "(nil)";
-  if (d->owner == NULL)
-    d->owner = "(nil)";
-
-  printf("Name: %s\nAge: %0.6f\nOwner: %s\n", d->name, d->age, d->owner);  
+if (!d)
+return;
+if (d->name == NULL)
+d->name = "(nil)";
+if (d->owner == NULL)
+d->owner = "(nil)";
+printf("Name: %s\nAge: %0.6f\nOwner: %s\n", d->name, d->age, d->owner);
 }
