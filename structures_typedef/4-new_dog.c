4569da331eee0b8683f6e0f6c80ada1acb44ad34
rico <someone@gmail.com>
2024-11-18 10:17:34 +0000

    4added

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -10,30 +10,45 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-  dog_t *new_dog; 
+  dog_t *new_dog;
+  int len_owner = 0;
+  int len_name = 0;
+  int i , j = 0; 
   
   if (name == NULL || owner == NULL)
     return (NULL);
+
+  while (name[len_name])
+    len_name++;
+
+  while (owner[len_owner])
+    len_owner++;
 
   new_dog = malloc(sizeof(dog_t));
   if (new_dog == NULL)
     return (NULL);
 
-  new_dog->name = strdup(name);
+  new_dog->name = malloc(len_name + 1);
   if (new_dog->name == NULL)
     {
-      free(new_dog);
-      return(NULL);
+      free (new_dog);
+      return (NULL); 
     }
-  
-  new_dog->owner = strdup(owner);
+
+  new_dog->owner = malloc(len_owner + 1);
   if (new_dog->owner == NULL)
     {
-      free(new_dog->name);
-      free(new_dog);
-      return(NULL);
+      free (new_dog->name);
+      free (new_dog);
+      return(NULL); 
     }
-  
+  for (i = 0; i <= len_name; i++)
+    new_dog->name[i] = name[i];
+
+  for (j = 0; j <= len_owner; j++)
+    new_dog->owner[j] = owner[j]; 
+
   new_dog->age = age;
+  
   return (new_dog); 
 }
