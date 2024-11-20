0a36e04a9ad2ab5fb6ebd4c7f986af5ee98975af
rico <someone@gmail.com>
2024-11-20 14:12:36 +0000

    syntax

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -26,20 +26,19 @@ return (a * b);
 }
 int op_div(int a, int b)
 {
-return (a / b);
 if (b == 0)
   {
     printf("Error\n");
     exit(100); 
-
   }
+ return (a / b); 
 }
 int op_mod(int a, int b)
 {
-return (a % b);
 if (b == 0)
   {
     printf("Error\n");
     exit (100); 
   }
+ return (a % b); 
 }
