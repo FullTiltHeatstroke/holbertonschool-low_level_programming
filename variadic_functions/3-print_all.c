5e486d17044d62ab9a89fd34cb8e760347e1dc8c
rico <someone@gmail.com>
2024-11-23 09:20:49 +0000

    3added

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -37,8 +37,9 @@ void print_all(const char * const format, ...)
     case 's':
       str = va_arg(args, char *);
       if (str == NULL)
+	{
 	printf("(nil)");
-      else
+	}
 	printf("%s", str);
       break;
     default:
