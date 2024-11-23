d0b9328b3a1ef6af05cf10d33166d0e92d1e604e
rico <someone@gmail.com>
2024-11-23 04:27:58 +0000

    3

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,16 @@
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_all(const char * const format, ...)
+{
+
+
+
+
+printf
+}
