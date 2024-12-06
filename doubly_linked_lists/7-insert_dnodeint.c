940d0bfa5c741fdbdbed74910ce9181b2291dee8
rico <10099@holbertonstudents.com>
2024-12-06 07:29:58 +0000

    7added

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -41,6 +41,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
   if(!temp->next)
     return(add_dnodeint_end(h, n));
 
+  new_node->prev = temp;
+  new_node->next = temp->next; 
   if(temp->next)
       temp->next->prev = new_node;
   temp->next = new_node;
